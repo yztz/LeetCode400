0ed83c1ddf1d57298827a0cb02242edcce15f489
aikhi007 <781089956@qq.com>
2022-05-13 23:12:06 +0800

    s9TODO一次编辑

---
+++ src/s9.java
@@ -0,0 +1,40 @@
+/* https://leetcode.cn/problems/one-away-lcci/ */
+public class s9 {
+    public boolean oneEditAway(String first, String second) {
+        int m = first.length(), n = second.length();
+        if (n - m == 1) {
+            return oneInsert(first, second);
+        } else if (m - n == 1) {
+            return oneInsert(second, first);
+        } else if (m == n) {
+            boolean foundDifference = false;
+            for (int i = 0; i < m; i++) {
+                if (first.charAt(i) != second.charAt(i)) {
+                    if (!foundDifference) {
+                        foundDifference = true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean oneInsert(String shorter, String longer) {
+        int length1 = shorter.length(), length2 = longer.length();
+        int index1 = 0, index2 = 0;
+        while (index1 < length1 && index2 < length2) {
+            if (shorter.charAt(index1) == longer.charAt(index2)) {
+                index1++;
+            }
+            index2++;
+            if (index2 - index1 > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
