d0e521821652ac00a03e91bd4cd415df49397e42
aikhi007 <781089956@qq.com>
2022-05-16 22:38:26 +0800

    s12☆☆非递归中序遍历|搜索树性质

--- src/s12.java
+++ src/s12.java
@@ -1,7 +1,22 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
-
+/* https://leetcode.cn/problems/successor-lcci/ */
 public class s12 {
 
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        TreeNode prev = null, curr = root;
+        while (!deque.isEmpty() || curr != null) {
+            while (curr != null) {
+                deque.push(curr);
+                curr = curr.left;
+            }
+            curr = deque.pop();
+            if (prev == p) return curr;
+            prev = curr;
+            curr = curr.right;
+        }
 
+        return null;
+    }
 }
