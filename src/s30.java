80c3543d1c28bf2da681902afd2be8c7902ee055
aikhi007 <781089956@qq.com>
2022-06-03 15:44:22 +0800

    s30☆☆☆数学|连续整数求和

---
+++ src/s30.java
@@ -0,0 +1,21 @@
+/* https://leetcode.cn/problems/consecutive-numbers-sum/ */
+public class s30 {
+    public int consecutiveNumbersSum(int n) {
+        int ans = 0;
+        int bound = 2 * n;
+        for (int k = 1; k * (k + 1) <= bound; k++) {
+            if (isKConsecutive(n, k)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    public boolean isKConsecutive(int n, int k) {
+        if (k % 2 == 1) {
+            return n % k == 0;
+        } else {
+            return n % k != 0 && 2 * n % k == 0;
+        }
+    }
+}
