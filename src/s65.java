cc9eb555ba19f8a10fd7343a3b4d9d223b3350cd
yztz <781089956@qq.com>
2022-07-13 18:06:16 +0800

    s65☆☆栈应用|行星碰撞

---
+++ src/s65.java
@@ -0,0 +1,26 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+/* https://leetcode.cn/problems/asteroid-collision/ */
+class s65 {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        for (int aster : asteroids) {
+            boolean alive = true;
+            while (alive && aster < 0 && !stack.isEmpty() && stack.peek() > 0) {
+                alive = stack.peek() < -aster; // aster 是否存在
+                if (stack.peek() <= -aster) {  // 栈顶行星爆炸
+                    stack.pop();
+                }
+            }
+            if (alive) {
+                stack.push(aster);
+            }
+        }
+        int size = stack.size();
+        int[] ans = new int[size];
+        for (int i = size - 1; i >= 0; i--) {
+            ans[i] = stack.pop();
+        }
+        return ans;
+    }
+}
