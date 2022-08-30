927680f65c8801b985e8b308198f21bd747b6df4
aikhi007 <781089956@qq.com>
2022-08-30 23:32:31 +0800

    s70☆最大二叉树 II|简单递归

---
+++ src/s70.java
@@ -0,0 +1,14 @@
+/* https://leetcode.cn/problems/maximum-binary-tree-ii/ */
+public class s70 {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        if(root.val <= val) {
+            TreeNode ans = new TreeNode(val);
+            ans.left = root;
+            return ans;
+        } else {
+            root.right = insertIntoMaxTree(root.right, val);
+            return root;
+        }
+    }
+}
