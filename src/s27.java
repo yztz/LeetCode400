4e28c3a929518eda9de5842156ce2d8651f442c9
aikhi007 <781089956@qq.com>
2022-09-26 00:18:24 +0800

    fix: comment

--- src/s27.java
+++ src/s27.java
@@ -1,33 +1,33 @@
 /* https://leetcode.cn/problems/Jf1JuT/ TODO */
 public class s27 {
-    public String alienOrder(String[] words) {
-        int[] bigger = new int[26];
-        int[] smaller = new int[26];
-        for (int i = 0; i < 26; i++) {
-            bigger[i] = i;
-            smaller[i] = i;
-        }
-        int n = words.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                String s = words[i], b = words[j];
-                if (s.equals(b)) {
-                    if (j > i + 1) return "";
-                    continue;
-                }
-                for (int k = 0; k < Math.min(s.length(), b.length()); k++) {
-                    int l = s.charAt(k) - 'a';
-                    int r = s.charAt(k) - 'a';
-                    if(l != r) {
-                        int t = bigger[l];
-                        while (t != r) {
-//                            t =
-                        }
-                        break;
-                    }
-                }
-
-            }
-        }
-    }
+//    public String alienOrder(String[] words) {
+//        int[] bigger = new int[26];
+//        int[] smaller = new int[26];
+//        for (int i = 0; i < 26; i++) {
+//            bigger[i] = i;
+//            smaller[i] = i;
+//        }
+//        int n = words.length;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = i + 1; j < n; j++) {
+//                String s = words[i], b = words[j];
+//                if (s.equals(b)) {
+//                    if (j > i + 1) return "";
+//                    continue;
+//                }
+//                for (int k = 0; k < Math.min(s.length(), b.length()); k++) {
+//                    int l = s.charAt(k) - 'a';
+//                    int r = s.charAt(k) - 'a';
+//                    if(l != r) {
+//                        int t = bigger[l];
+//                        while (t != r) {
+////                            t =
+//                        }
+//                        break;
+//                    }
+//                }
+//
+//            }
+//        }
+//    }
 }
